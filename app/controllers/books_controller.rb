188ac133b843f410c5bbb5748486964c3e9928e8
tkykkkkk <158381693+tkykkkkk@users.noreply.github.com>
2024-03-30 15:11:55 +0000

    [Finish]Oyo-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,13 @@ class BooksController < ApplicationController
     @new_book = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+    
   end
 
   def create
